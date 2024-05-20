03a7a6e1932e296dd13c177dd55f2ab23cbb794a
CarllosHenrique <crick.lucas@gmail.com>
2024-05-19 23:01:56 -0300

    [0.2.1]:[refactor]: Melhorando os locales .yml e colocando o botao de back nas paginas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
       model.errors.full_messages_for(attribute).join(", ")
     end
   end
+
+  def back_button(path, position = "none")
+    link_to path, class: "btn btn-outline-secondary", style: "float: #{position};" do
+      content_tag(:i, "", class: "fa-solid fa-arrow-left")
+    end
+  end
 end
